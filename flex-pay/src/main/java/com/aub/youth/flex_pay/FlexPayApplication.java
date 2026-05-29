0f9b7dce5d9c0d3ace1a8de1938205a825623ef6
PhySophavat <physophavat@gmail.com>
2026-05-29 16:37:27 +0700

    do with done

---
+++ flex-pay/src/main/java/com/aub/youth/flex_pay/FlexPayApplication.java
@@ -0,0 +1,13 @@
+package com.aub.youth.flex_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexPayApplication.class, args);
+	}
+
+}
---
+++ flex-pay/src/test/java/com/aub/youth/flex_pay/FlexPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aub.youth.flex_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
